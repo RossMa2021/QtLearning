c99f27716eb19af67096cbb78bb02fc33b70db3d
younger_ma <mawenbo2015@163.com>
2019-01-17 18:32:03 +0800

    8
    8

--- myGame/main.cpp
+++ myGame/main.cpp
@@ -1,11 +1,14 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include "myview.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+    //MainWindow w;
+    //w.show();
+    MyView m;
+    m.show();
 
     return a.exec();
 }
--- myGame/myview.h
+++ myGame/myview.h
@@ -8,8 +8,28 @@ class BoxGroup;
 
 class MyView : public QGraphicsView
 {
+   Q_OBJECT
 public:
-    MyView();
+     explicit MyView(QWidget *parent = 0);
+
+public slots:
+    void startGame();
+    void clearFullRows();
+    void moveBox();
+    void gameOver();
+
+private:
+    BoxGroup *boxGroup;
+    BoxGroup *nextBoxGroup;
+    QGraphicsLineItem *topLine;
+    QGraphicsLineItem *bottomLine;
+    QGraphicsLineItem *leftLine;
+    QGraphicsLineItem *rightLine;
+    qreal gameSpeed;
+    QList<int> rows;
+    void initView();
+    void initGame();
+    void updateScore(const int fullRowNum = 0);
 };
 
 #endif // MYVIEW_H

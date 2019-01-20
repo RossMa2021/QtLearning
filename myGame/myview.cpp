8be0f2829c567a62ce4a0fdeb07445dc8ec94abc
youngerma <mawenbo2015@163.com>
2019-01-20 16:06:55 +0800

    add menu button
    add menu button

--- myGame/myview.cpp
+++ myGame/myview.cpp
@@ -8,6 +8,14 @@
 #include <QGraphicsBlurEffect>
 #include <QTimer>
 
+// 在2-4中添加的代码
+#include <QPushButton>
+#include <QGraphicsProxyWidget>
+#include <QApplication>
+#include <QLabel>
+#include <QFileInfo>
+#include <QGraphicsTextItem>
+
 // 游戏的初始速度
 static const qreal INITSPEED = 500;
 
@@ -55,12 +63,173 @@ void MyView::initView()
     //testBoxGroup = new BoxGroup;
     //scene->addItem(testBoxGroup);
 
-    startGame();
+    //startGame();
+    // 在2-3中添加的代码
+
+    // 得分文本
+    gameScoreText = new QGraphicsTextItem(0);
+    gameScoreText->setFont(QFont("Times", 20, QFont::Bold));
+    gameScoreText->setPos(650, 350);
+
+    // 级别文本
+    gameLevelText = new QGraphicsTextItem(0);
+    gameLevelText->setFont(QFont("Times", 30, QFont::Bold));
+    gameLevelText->setPos(20, 150);
+
+
+
+    /*****************下面是2-4中添加的代码，部分代码在书中省略了*************/
+
+    // 设置初始为隐藏状态
+    topLine->hide();
+    bottomLine->hide();
+    leftLine->hide();
+    rightLine->hide();
+    gameScoreText->hide();
+    gameLevelText->hide();
+
+    // 黑色遮罩
+    QWidget *mask = new QWidget;
+    mask->setAutoFillBackground(true);
+    mask->setPalette(QPalette(QColor(0, 0, 0, 80)));
+    mask->resize(900, 600);
+    maskWidget = scene->addWidget(mask);
+    maskWidget->setPos(-50, -50);
+    // 设置其Z值为1，这样可以处于Z值为0的图形项上面
+    maskWidget->setZValue(1);
+
+    // 选项面板
+    QWidget *option = new QWidget;
+    QPushButton *optionCloseButton = new QPushButton(tr("关   闭"), option);
+    QPalette palette;
+    palette.setColor(QPalette::ButtonText, Qt::black);
+    optionCloseButton->setPalette(palette);
+    optionCloseButton->move(120, 300);
+    connect(optionCloseButton, SIGNAL(clicked()), option, SLOT(hide()));
+    option->setAutoFillBackground(true);
+    option->setPalette(QPalette(QColor(0, 0, 0, 180)));
+    option->resize(300, 400);
+    QGraphicsWidget *optionWidget = scene->addWidget(option);
+    optionWidget->setPos(250, 50);
+    optionWidget->setZValue(3);
+    optionWidget->hide();
+
+    // 帮助面板
+    QWidget *help = new QWidget;
+    QPushButton *helpCloseButton = new QPushButton(tr("关   闭"), help);
+    helpCloseButton->setPalette(palette);
+    helpCloseButton->move(120, 300);
+    connect(helpCloseButton, SIGNAL(clicked()), help, SLOT(hide()));
+    help->setAutoFillBackground(true);
+    help->setPalette(QPalette(QColor(0, 0, 0, 180)));
+    help->resize(300, 400);
+    QGraphicsWidget *helpWidget = scene->addWidget(help);
+    helpWidget->setPos(250, 50);
+    helpWidget->setZValue(3);
+    helpWidget->hide();
+
+    QLabel *helpLabel = new QLabel(help);
+    helpLabel->setText(tr("<h1><font color=white>yafeilinux作品"
+                          "<br>www.yafeilinux.com</font></h1>"));
+    helpLabel->setAlignment(Qt::AlignCenter);
+    helpLabel->move(30, 150);
+
+
+    // 游戏欢迎文本
+    gameWelcomeText = new QGraphicsTextItem(0);
+    gameWelcomeText->setHtml(tr("<font color=white>MyBox方块游戏</font>"));
+    gameWelcomeText->setFont(QFont("Times", 30, QFont::Bold));
+    gameWelcomeText->setPos(250, 100);
+    gameWelcomeText->setZValue(2);
+
+    // 游戏暂停文本
+    gamePausedText = new QGraphicsTextItem(0);
+    gamePausedText->setHtml(tr("<font color=white>游戏暂停中！</font>"));
+    gamePausedText->setFont(QFont("Times", 30, QFont::Bold));
+    gamePausedText->setPos(300, 100);
+    gamePausedText->setZValue(2);
+    gamePausedText->hide();
+
+    // 游戏结束文本
+    gameOverText = new QGraphicsTextItem(0);
+    gameOverText->setHtml(tr("<font color=white>游戏结束！</font>"));
+    gameOverText->setFont(QFont("Times", 30, QFont::Bold));
+    gameOverText->setPos(320, 100);
+    gameOverText->setZValue(2);
+    gameOverText->hide();
+
+
+    // 游戏中使用的按钮
+
+    QPushButton *button1 = new QPushButton(tr("开    始"));
+    QPushButton *button2 = new QPushButton(tr("选    项"));
+    QPushButton *button3 = new QPushButton(tr("帮    助"));
+    QPushButton *button4 = new QPushButton(tr("退    出"));
+    QPushButton *button5 = new QPushButton(tr("重新开始"));
+    QPushButton *button6 = new QPushButton(tr("暂    停"));
+    QPushButton *button7 = new QPushButton(tr("主 菜 单"));
+    QPushButton *button8 = new QPushButton(tr("返回游戏"));
+    QPushButton *button9 = new QPushButton(tr("结束游戏"));
+
+
+    connect(button1, SIGNAL(clicked()), this, SLOT(startGame()));
+    connect(button2, SIGNAL(clicked()), option, SLOT(show()));
+    connect(button3, SIGNAL(clicked()), help, SLOT(show()));
+    connect(button4, SIGNAL(clicked()), qApp, SLOT(quit()));
+    connect(button5, SIGNAL(clicked()), this, SLOT(restartGame()));
+    connect(button6, SIGNAL(clicked()), this, SLOT(pauseGame()));
+    connect(button7, SIGNAL(clicked()), this, SLOT(finishGame()));
+    connect(button8, SIGNAL(clicked()), this, SLOT(returnGame()));
+    connect(button9, SIGNAL(clicked()), this, SLOT(finishGame()));
+
+    startButton = scene->addWidget(button1);
+    optionButton = scene->addWidget(button2);
+    helpButton = scene->addWidget(button3);
+    exitButton = scene->addWidget(button4);
+    restartButton = scene->addWidget(button5);
+    pauseButton = scene->addWidget(button6);
+    showMenuButton = scene->addWidget(button7);
+    returnButton = scene->addWidget(button8);
+    finishButton = scene->addWidget(button9);
+
+    startButton->setPos(370, 200);
+    optionButton->setPos(370, 250);
+    helpButton->setPos(370, 300);
+    exitButton->setPos(370, 350);
+    restartButton->setPos(600, 150);
+    pauseButton->setPos(600, 200);
+    showMenuButton->setPos(600, 250);
+    returnButton->setPos(370, 200);
+    finishButton->setPos(370, 250);
+
+    startButton->setZValue(2);
+    optionButton->setZValue(2);
+    helpButton->setZValue(2);
+    exitButton->setZValue(2);
+    restartButton->setZValue(2);
+    returnButton->setZValue(2);
+    finishButton->setZValue(2);
+
+    restartButton->hide();
+    finishButton->hide();
+    pauseButton->hide();
+    showMenuButton->hide();
+    returnButton->hide();
+
+    /*****************上面是2-4中添加的代码，部分代码在书中省略了*************/
 }
 
 // 开始游戏
 void MyView::startGame()
 {
+    // 在2-4中添加的代码
+    gameWelcomeText->hide();
+    startButton->hide();
+    optionButton->hide();
+    helpButton->hide();
+    exitButton->hide();
+    maskWidget->hide();
+
     initGame();
 }
 
@@ -72,6 +241,24 @@ void MyView::initGame()
     boxGroup->startTimer(INITSPEED);
     gameSpeed = INITSPEED;
     nextBoxGroup->createBox(QPointF(500, 70));
+
+    // 在2-3中添加的代码
+    scene()->setBackgroundBrush(QPixmap(":/images/background01.png"));
+    gameScoreText->setHtml(tr("<font color=red>0</font>"));
+    gameLevelText->setHtml(tr("<font color=white>第<br>一<br>幕</font>"));
+
+    // 在2-4中添加的代码
+    restartButton->show();
+    pauseButton->show();
+    showMenuButton->show();
+    gameScoreText->show();
+    gameLevelText->show();
+    topLine->show();
+    bottomLine->show();
+    leftLine->show();
+    rightLine->show();
+    // 可能以前返回主菜单时隐藏了boxGroup
+    boxGroup->show();
 
     //testBoxGroup->createBox(QPointF(300, 150));
 }
@@ -144,12 +331,129 @@ void MyView::moveBox()
 }
 
 
+// 更新分数
 void MyView::updateScore(const int fullRowNum)
 {
-    // 更新分数
+    int score = fullRowNum * 100;
+    int currentScore = gameScoreText->toPlainText().toInt();
+    currentScore += score;
+    // 显示当前分数
+    gameScoreText->setHtml(tr("<font color=red>%1</font>").arg(currentScore));
+    // 判断级别
+    if (currentScore < 500) {
+        // 第一级,什么都不用做
+    } else if (currentScore < 1000) { // 第二级
+         gameLevelText->setHtml(tr("<font color=white>第<br>二<br>幕</font>"));
+         scene()->setBackgroundBrush(QPixmap(":/images/background02.png"));
+         gameSpeed = 300;
+         boxGroup->stopTimer();
+         boxGroup->startTimer(gameSpeed);
+    } else {
+        // 添加下一个级别的设置
+    }
 }
 
+// 游戏结束
 void MyView::gameOver()
 {
-    // 游戏结束
+    pauseButton->hide();
+    showMenuButton->hide();
+    maskWidget->show();
+    gameOverText->show();
+    restartButton->setPos(370, 200);
+    finishButton->show();
+}
+
+// 重新开始游戏
+void MyView::restartGame()
+{
+    maskWidget->hide();
+    gameOverText->hide();
+    finishButton->hide();
+    restartButton->setPos(600, 150);
+
+    // 销毁提示方块组和当前方块移动区域中的所有小方块
+    nextBoxGroup->clearBoxGroup(true);
+    boxGroup->clearBoxGroup();
+    boxGroup->hide();
+    foreach (QGraphicsItem *item, scene()->items(QRectF(199, 49, 202, 402), Qt::ContainsItemShape)) {
+        // 先从场景中移除小方块，因为使用deleteLater()是在返回主事件循环后才销毁
+        // 小方块的，为了在出现新的方块组时不发生碰撞，所以需要先从场景中移除小方块
+        scene()->removeItem(item);
+        OneBox *box = (OneBox*) item;
+        box->deleteLater();
+    }
+
+    initGame();
+}
+
+// 结束当前游戏
+void MyView::finishGame()
+{
+    gameOverText->hide();
+    finishButton->hide();
+    restartButton->setPos(600, 150);
+    restartButton->hide();
+    pauseButton->hide();
+    showMenuButton->hide();
+    gameScoreText->hide();
+    gameLevelText->hide();
+
+    topLine->hide();
+    bottomLine->hide();
+    leftLine->hide();
+    rightLine->hide();
+
+    nextBoxGroup->clearBoxGroup(true);
+    boxGroup->clearBoxGroup();
+    boxGroup->hide();
+
+    foreach (QGraphicsItem *item, scene()->items(QRectF(199, 49, 202, 402), Qt::ContainsItemShape)) {
+        OneBox *box = (OneBox*) item;
+        box->deleteLater();
+    }
+
+    // 可能是在进行游戏时按下“主菜单”按钮
+    maskWidget->show();
+    gameWelcomeText->show();
+    startButton->show();
+    optionButton->show();
+    helpButton->show();
+    exitButton->show();
+    scene()->setBackgroundBrush(QPixmap(":/images/background.png"));
+}
+
+// 暂停游戏
+void MyView::pauseGame()
+{
+    boxGroup->stopTimer();
+    restartButton->hide();
+    pauseButton->hide();
+    showMenuButton->hide();
+    maskWidget->show();
+    gamePausedText->show();
+    returnButton->show();
+}
+
+
+// 返回游戏，处于暂停状态时
+void MyView::returnGame()
+{
+    returnButton->hide();
+    gamePausedText->hide();
+    maskWidget->hide();
+    restartButton->show();
+    pauseButton->show();
+    showMenuButton->show();
+    boxGroup->startTimer(gameSpeed);
+}
+
+// 如果正在进行游戏，当键盘按下时总是方块组获得焦点
+void MyView::keyPressEvent(QKeyEvent *event)
+{
+    if (pauseButton->isVisible())
+        boxGroup->setFocus();
+    else
+        boxGroup->clearFocus();
+    QGraphicsView::keyPressEvent(event);
 }
